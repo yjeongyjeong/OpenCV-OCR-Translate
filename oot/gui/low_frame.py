88f9b3f8771106740ded213edbb2aca6a2758cc9
yjeongyjeong <dbswjd4991@naver.com>
2024-07-07 18:52:35 +0900

    refactor: middle frame, low frame 초기화 작업 완료

--- oot/gui/low_frame.py
+++ oot/gui/low_frame.py
@@ -17,7 +17,31 @@ from tkinter import END, scrolledtext
 class LowFrame:
 
     def __init__(self, root):
-        pass
+        # create a notebook
+        low_frm = ttk.Notebook(root)
+        LowFrame.notebook = low_frm
+        low_frm.bind("<<NotebookTabChanged>>", self.__tabChanged)
+        
+        low_frm.pack(pady=10, fill='both')
+        low_frm.config(height=220)
+
+        # create frames
+        remove_tab = ttk.Frame(low_frm, height=50)
+        write_tab = ttk.Frame(low_frm, height=50)
+        edit_tab = ttk.Frame(low_frm, height=50)
+        mosaic_tab = ttk.Frame(low_frm, height=50)
+
+        remove_tab.pack(fill='both', expand=True)
+        write_tab.pack(fill='both', expand=True)
+        edit_tab.pack(fill='both', expand=True)
+        mosaic_tab.pack(fill='both', expand=True)
+
+
+        # add frames to notebook
+        low_frm.add(remove_tab, text='지우기')
+        low_frm.add(write_tab, text='쓰기')
+        low_frm.add(edit_tab, text='이미지편집')
+        low_frm.add(mosaic_tab, text='모자이크')
 
     @classmethod
     def getStatusOfCheckListInRemoveTab(cls, idx):
@@ -38,18 +62,7 @@ class LowFrame:
     @classmethod
     def resetColorOfButtonInWriteTab(cls, color='#FFFF00'):
         pass
-
-from enum import Enum, auto
-class ScrollableListType(Enum):
-    CHECK_BUTTON = auto()
-    RADIO_BUTTON = auto()
-
-class ScrollableList(tk.Frame):
-    def __init__(self, root, list_type, *args, **kwargs):
-        pass
-
-    def __getIndexedText(self, idx, text):
-        pass
     
-    def reset(self, text_list=None):
-        pass
+    def __tabChanged(self, event):
+        tab_idx = LowFrame.notebook.index(LowFrame.notebook.select())
+        print(f'tab_idx={tab_idx}')
--- oot/gui/middle_frame.py
+++ oot/gui/middle_frame.py
@@ -1,6 +1,57 @@
+import os
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
+
+from oot.data.data_manager import DataManager
+from oot.gui.low_frame import LowFrame
+
+class CanvasWorker:
+    def __init__(self, img_file, canvas):
+        self.img_file = img_file
+        self.canvas = canvas
+        self.image = Image.open(img_file)
+        self.photoimage = None
+    
+    def drawImage(self):
+        self.photoimage = ImageTk.PhotoImage(file=self.img_file)
+        
+        canvas_w = self.canvas.winfo_width()
+        canvas_h = self.canvas.winfo_height()
+        # sometimes at the very first draw, the canvas size was (1,1) and it makes error
+        if canvas_w <= 1 or canvas_h <= 1:
+            return
+
+        self.scale_ratio = 1.0
+        if canvas_w >= self.photoimage.width() and canvas_h >= self.photoimage.height():
+            self.canvas.create_image(0,0, image=self.photoimage, anchor="nw")
+        else:
+            w1, h1 = self.image.size
+            w, h = CanvasWorker.getAdaptedImageSize(w1, h1, canvas_w, canvas_h)
+            image_resized = self.image.resize((w, h), Image.ANTIALIAS)
+            self.photoimage = ImageTk.PhotoImage(image_resized)
+            self.canvas.create_image(0,0, image=self.photoimage, anchor="nw")
+            self.scale_ratio = w/w1
+            
+    def changeImageFile(self, img_file):
+        self.img_file = img_file
+        self.image = Image.open(img_file)
+
+    def setImage(self, image):
+        if image is not None:
+            self.image = image
+    
+    def getImage(self):
+        return self.image
+    
+    @classmethod
+    def getAdaptedImageSize(cls, img_w, img_h, canvas_w, canvas_h):
+        w_ratio = img_h/img_w
+        h_ratio = img_w/img_h
+        if canvas_w*w_ratio <= canvas_h:
+            return int(canvas_w), int(canvas_w*w_ratio)
+        else:
+            return int(canvas_h*h_ratio), int(canvas_h)
 
 
 #------------------------------------------------------------------------------
@@ -8,7 +59,37 @@ from PIL import ImageTk, Image
 #------------------------------------------------------------------------------
 class MiddleFrame:
     def __init__(self, root):
-        pass
+        # middle canvases frame
+        mid_frm = tk.Frame(root)
+        MiddleFrame.mid_frm = mid_frm
+        mid_frm.pack(padx=2, pady=2, fill='both', expand=True)
+
+        left_canvas = tk.Canvas(mid_frm, bg='lightgray')
+        left_canvas.grid(row=0, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
+        right_canvas = tk.Canvas(mid_frm, bg='lightgray')
+        right_canvas.grid(row=0, column=1, sticky=tk.E+tk.W+tk.N+tk.S)
+
+        img_folder = DataManager.folder_data.folder
+        src_file = DataManager.folder_data.work_file.name
+        # TODO: 데이터매니저에서 outfile을 리턴해주는 함수 필요함
+        out_file = src_file
+
+        # Reference :
+        # - https://www.youtube.com/watch?v=xiGQD2J47nA
+        # - https://github.com/flatplanet/Intro-To-TKinter-Youtube-Course/blob/master/image_bg_resize.py
+        # - https://github.com/flatplanet/Intro-To-TKinter-Youtube-Course/
+        def resizer(e):
+            MiddleFrame.redrawCanvasImages()
+ 
+        left_canvas.bind('<Configure>', resizer)
+        # Added by Q&A from stackoverflow
+        # https://stackoverflow.com/questions/72713209/how-to-keep-two-textboxes-the-same-size-when-resizing-window-with-python-tkinter
+        mid_frm.columnconfigure((0,1), weight=1)
+        mid_frm.rowconfigure(0, weight=1)
+
+        MiddleFrame.src_canvas_worker = CanvasWorker(src_file, left_canvas)
+        MiddleFrame.out_canvas_worker = CanvasWorker(out_file, right_canvas)
+
 
     @classmethod
     def redrawCanvasImages(cls):
