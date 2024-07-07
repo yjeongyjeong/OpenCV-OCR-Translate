43dc1745b093ff4ac9f8bddc9cb60fb058962272
JiwonHwang84 <ghkdwldnjs112@gmail.com>
2024-07-07 18:59:42 +0900

    Add: 작업경로 표시 추가

--- oot/gui/top_frame.py
+++ oot/gui/top_frame.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+from oot.data.data_manager import DataManager
+
+
 
 #------------------------------------------------------------------------------
 # Top frame : top side buttons layout and command handlers
@@ -14,9 +17,10 @@ class TopFrame:
 
         # top buttons : '이전 이미지', '다음 이미지', '폴더 변경'
         btn_change_folder = ttk.Button(top_frm, text='폴더 변경')
+        
 
         label_curr_file_title = ttk.Label(top_frm, text='작업 파일:')
-        label_curr_file_name = ttk.Label(top_frm, text='')
+        label_curr_file_name = ttk.Label(top_frm, text=DataManager.folder_data.work_file.name)
 
         btn_change_folder.pack(side='left')
         label_curr_file_title.pack(side='left')
