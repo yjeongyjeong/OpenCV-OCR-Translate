679d9d7e5390445e24c42cef6da86c075de5971c
JiwonHwang84 <ghkdwldnjs112@gmail.com>
2024-07-09 22:12:45 +0900

    Add:  "추천 상업용 문구" 텍스트 수정

--- oot/gui/low_frame.py
+++ oot/gui/low_frame.py
@@ -178,7 +178,7 @@ class LowFrame:
         write_tab_text_google = tk.Text(write_tab_trans_frm, height=3)
         write_tab_text_google.grid(column=0, row=3, sticky=tk.W+tk.E+tk.N+tk.S)
 
-        write_tab_label_final = ttk.Label(write_tab_trans_frm, text='적용할 텍스트 :')
+        write_tab_label_final = ttk.Label(write_tab_trans_frm, text='추천 상업용 문구 :')
         write_tab_label_final.grid(column=0, row=4, sticky=tk.W)
         write_tab_text_final = tk.Text(write_tab_trans_frm, height=3)
         write_tab_text_final.grid(column=0, row=5, sticky=tk.W+tk.E+tk.N+tk.S)
